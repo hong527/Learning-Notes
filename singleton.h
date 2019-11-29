15be5ad2735a958c9d12271160b810a5fbcde51d
hong <1636775032@qq.com>
2019-11-29 19:58:38 +0800

    单例模式

---
+++ singleton.h
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <mutex>
+#include <stdlib.h>
+
+class MySingleton
+{
+public:
+	~MySingleton()=default;
+
+	MySingleton* getInstance()
+	{
+		if (!sInstance)
+		{
+			sInstance = new MySingleton();
+			static GCMySingleton gCMySingleton;
+		}
+		return sInstance;
+	}
+	class GCMySingleton
+	{
+	public:
+		GCMySingleton() = default;
+		~GCMySingleton();
+	private:
+	};
+
+private:
+	MySingleton() = default;
+
+	static MySingleton* sInstance;
+};
+MySingleton* MySingleton::sInstance = nullptr;
+
+MySingleton::GCMySingleton::~GCMySingleton()
+{
+	if (MySingleton::sInstance)
+	{
+		delete MySingleton::sInstance;
+		MySingleton::sInstance = nullptr;
+	}
+}
